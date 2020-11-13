1ef7300bfa2bc59ad5ae8a392f7139733aa50a63
FabioJrSilva <fabiopt6@gmail.com>
2020-11-12 22:56:39 -0300

    Adicionar type MailerOptions para mailerConfig

--- src/configs/mailer.config.ts
+++ src/configs/mailer.config.ts
@@ -1,21 +1,26 @@
+import { MailerOptions } from '@nestjs-modules/mailer';
 import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 import * as path from 'path';
 
-export default () => ({
-  template: {
-    dir: path.resolve(__dirname, '..', '..', 'templates'),
-    adapter: new HandlebarsAdapter(),
-    options: {
-      extName: '.hbs',
-      layoutsDir: path.resolve(__dirname, '..', '..', 'templates'),
+export default () => {
+  const mailerConfig: MailerOptions = {
+    template: {
+      dir: path.resolve(__dirname, '..', '..', 'templates'),
+      adapter: new HandlebarsAdapter(),
+      options: {
+        extName: '.hbs',
+        layoutsDir: path.resolve(__dirname, '..', '..', 'templates'),
+      },
     },
-  },
-  transport: {
-    host: process.env.MAIL_HOST,
-    port: process.env.MAIL_PORT,
-    auth: {
-      user: process.env.MAIL_AUTH_USER,
-      pass: process.env.MAIL_AUTH_PASS
+    transport: {
+      host: process.env.MAIL_HOST,
+      port: process.env.MAIL_PORT,
+      auth: {
+        user: process.env.MAIL_AUTH_USER,
+        pass: process.env.MAIL_AUTH_PASS
+      }
     }
   }
-});
+
+  return mailerConfig
+}
